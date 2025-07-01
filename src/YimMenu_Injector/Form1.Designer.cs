d5799a62956e1ad40d27f3cdafa82596a72a15dd
KRDPKK <Benjamin.Molla@yahoo.com>
2025-07-01 23:54:44 +0200

    Add files via upload

---
+++ src/YimMenu_Injector/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace YY
+{
+    partial class yy
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button btnInject;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(yy));
+            this.btnInject = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnInject
+            // 
+            this.btnInject.Location = new System.Drawing.Point(12, 330);
+            this.btnInject.Name = "btnInject";
+            this.btnInject.Size = new System.Drawing.Size(64, 44);
+            this.btnInject.TabIndex = 0;
+            this.btnInject.Text = "Inject";
+            this.btnInject.UseVisualStyleBackColor = true;
+            this.btnInject.Click += new System.EventHandler(this.btnInject_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(82, 330);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(203, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "YimMenU link";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(399, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(364, 384);
+            this.label1.TabIndex = 2;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(51, 54);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(385, 158);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "About Me:\r\nI\'m an under-18 developer focused on making quality,\r\nopen-source GTA " +
+    "V cheats for educational use.\r\n\r\nNOTE: This is only a injector full creds to yim" +
+    "menuv2 and its creators.\r\n";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(385, 71);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "How to Setup: Click the Yimmenu link button then dowload the dll and place it in " +
+    "the same directory as this application is thats it youre ready to go!";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(291, 330);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 44);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Injector Github link";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(652, 376);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnInject);
+            this.Name = "Form1";
+            this.Text = "YimMenu Injector";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ src/YimMenu_Injector/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace YY
+{
+    public partial class yy : Form
+    {
+        public yy() => InitializeComponent();
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr OpenProcess(uint access, bool inheritHandle, int processId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, uint size, uint allocType, uint protect);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] buffer, uint size, out IntPtr written);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes,
+            uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+
+        [DllImport("ntdll.dll", SetLastError = true)]
+        static extern uint NtCreateThreadEx(
+            out IntPtr threadHandle,
+            uint desiredAccess,
+            IntPtr objectAttributes,
+            IntPtr processHandle,
+            IntPtr startAddress,
+            IntPtr parameter,
+            bool createSuspended,
+            uint stackZeroBits,
+            uint sizeOfStackCommit,
+            uint sizeOfStackReserve,
+            IntPtr bytesBuffer);
+
+        const uint PROCESS_ALL_ACCESS = 0x1F0FFF;
+        const uint MEM_COMMIT = 0x1000;
+        const uint PAGE_READWRITE = 0x04;
+        const uint THREAD_ALL_ACCESS = 0x1FFFFF;
+
+        enum InjectionMethod
+        {
+            CreateRemoteThread = 1,
+            NtCreateThreadEx = 2
+        }
+
+        private void btnInject_Click(object sender, EventArgs e)
+        {
+            string dllPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "YimMenu.dll");
+            if (!File.Exists(dllPath))
+            {
+                MessageBox.Show("YimMenu.dll not found!");
+                return;
+            }
+
+            Process[] targets = Process.GetProcessesByName("GTA5_Enhanced");
+            if (targets.Length == 0)
+            {
+                MessageBox.Show("GTA5_Enhanced.exe not running.");
+                return;
+            }
+
+            // Prompt user to choose injection method
+            var choice = MessageBox.Show("Choose injection method:\nYes = CreateRemoteThread\nNo = NtCreateThreadEx",
+                                         "Select Injection Method", MessageBoxButtons.YesNoCancel);
+
+            if (choice == DialogResult.Cancel)
+                return;
+
+            InjectionMethod injectionMethod = (choice == DialogResult.Yes) ? InjectionMethod.CreateRemoteThread : InjectionMethod.NtCreateThreadEx;
+
+            int successCount = 0;
+            int failCount = 0;
+
+            foreach (Process gta in targets)
+            {
+                bool injected = false;
+
+                IntPtr hProcess = OpenProcess(PROCESS_ALL_ACCESS, false, gta.Id);
+                if (hProcess == IntPtr.Zero)
+                {
+                    failCount++;
+                    continue;
+                }
+
+                IntPtr allocMem = VirtualAllocEx(hProcess, IntPtr.Zero, (uint)(dllPath.Length + 1), MEM_COMMIT, PAGE_READWRITE);
+                if (allocMem == IntPtr.Zero)
+                {
+                    failCount++;
+                    continue;
+                }
+
+                byte[] bytes = Encoding.ASCII.GetBytes(dllPath);
+                if (!WriteProcessMemory(hProcess, allocMem, bytes, (uint)bytes.Length, out _))
+                {
+                    failCount++;
+                    continue;
+                }
+
+                IntPtr loadLibAddr = GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+                if (loadLibAddr == IntPtr.Zero)
+                {
+                    failCount++;
+                    continue;
+                }
+
+                if (injectionMethod == InjectionMethod.CreateRemoteThread)
+                {
+                    IntPtr hThread = CreateRemoteThread(hProcess, IntPtr.Zero, 0, loadLibAddr, allocMem, 0, IntPtr.Zero);
+                    if (hThread != IntPtr.Zero)
+                        injected = true;
+                }
+                else if (injectionMethod == InjectionMethod.NtCreateThreadEx)
+                {
+                    injected = NtCreateThreadExInject(hProcess, loadLibAddr, allocMem);
+                }
+
+                if (injected)
+                    successCount++;
+                else
+                    failCount++;
+            }
+
+            MessageBox.Show($"Injection finished.\nSuccess: {successCount}\nFailed: {failCount}");
+        }
+
+        private bool NtCreateThreadExInject(IntPtr hProcess, IntPtr loadLibraryAddr, IntPtr allocMem)
+        {
+            IntPtr threadHandle;
+            uint status = NtCreateThreadEx(
+                out threadHandle,
+                THREAD_ALL_ACCESS,
+                IntPtr.Zero,
+                hProcess,
+                loadLibraryAddr,
+                allocMem,
+                false,
+                0,
+                0,
+                0,
+                IntPtr.Zero);
+
+            return status == 0 && threadHandle != IntPtr.Zero;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/YimMenu/YimMenuV2/releases/tag/nightly");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/KRDPKK/YimMenuV2-Injector");
+        }
+    }
+}
