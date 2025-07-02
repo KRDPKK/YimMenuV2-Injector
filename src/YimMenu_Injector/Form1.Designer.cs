5e704afa292276f7d439fbcaaab798dbd4b4f5f4
KRDPKK <Benjamin.Molla@yahoo.com>
2025-07-02 11:48:58 +0200

    Update Form1.Designer.cs

--- src/YimMenu_Injector/Form1.Designer.cs
+++ src/YimMenu_Injector/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace YY
+namespace YY
 {
     partial class yy
     {
@@ -22,11 +22,13 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnInject
             // 
+            this.btnInject.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnInject.Location = new System.Drawing.Point(12, 330);
             this.btnInject.Name = "btnInject";
             this.btnInject.Size = new System.Drawing.Size(64, 44);
@@ -37,18 +39,19 @@
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(82, 330);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(203, 44);
             this.button1.TabIndex = 1;
-            this.button1.Text = "YimMenU link";
+            this.button1.Text = "YimMenu link";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(399, 0);
+            this.label1.Location = new System.Drawing.Point(403, -1);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(364, 384);
             this.label1.TabIndex = 2;
@@ -66,7 +69,7 @@
             // label2
             // 
             this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(8, 69);
+            this.label2.Location = new System.Drawing.Point(12, 69);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(385, 158);
             this.label2.TabIndex = 4;
@@ -78,15 +81,16 @@
             // label3
             // 
             this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(12, 227);
+            this.label3.Location = new System.Drawing.Point(12, 243);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(385, 71);
             this.label3.TabIndex = 5;
-            this.label3.Text = "How to Setup: Click the Yimmenu link button then dowload the dll and place it in " +
-    "the same directory as this application is thats it youre ready to go!";
+            this.label3.Text = resources.GetString("label3.Text");
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // button2
             // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.Location = new System.Drawing.Point(291, 330);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(106, 44);
@@ -95,9 +99,20 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // Form1
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(444, 192);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "-nobattleye";
+            // 
+            // yy
             // 
             this.ClientSize = new System.Drawing.Size(652, 376);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -105,10 +120,11 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnInject);
-            this.Name = "Form1";
+            this.Name = "yy";
             this.Text = "YimMenu Injector";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -118,5 +134,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
